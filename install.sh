454faeb1624c1a8d60430e37a6bc3d084c0b7d38
shepard <eviltomorrow@163.com>
2022-02-09 21:02:02 +0800

    dev

--- install.sh
+++ install.sh
@@ -7,4 +7,9 @@ npm install hexo-renderer-kramed --save
 npm uninstall hexo-math --save
 npm install hexo-renderer-mathjax --save
 
-sed -i 's/<script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>/<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-MML-AM_CHTML"></script>/g' blog/node_modules/hexo-renderer-mathjax/mathjax.html
+
+# 文件：blog/node_modules/hexo-renderer-mathjax/mathjax.html
+# 替换 
+# 	<script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+# 	=》 
+# 	<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-MML-AM_CHTML"></script>/g' 
