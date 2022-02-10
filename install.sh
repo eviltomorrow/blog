67f9781512ea173958869d557678b6c7d6ca753f
shepard <eviltomorrow@163.com>
2022-02-10 08:33:53 +0800

    dev

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ npm install hexo-renderer-mathjax --save
 # 替换 
 # 	<script src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
 # 	=》 
-# 	<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-MML-AM_CHTML"></script>/g' 
+# 	<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.6/MathJax.js?config=TeX-MML-AM_CHTML"></script>
